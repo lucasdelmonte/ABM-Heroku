d910eba45a0b847df0a10711badcb527ade1ec17
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-10-08 18:39:47 -0300

    Se corrigen tipos de errores y validaciones en busquedas por ID, email, firtstnamy y lastname

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const conexion_bd = process.env.DB_MONGODB;
@@ -9,7 +8,7 @@ const conexion_bd = process.env.DB_MONGODB;
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 mongoose
   .connect(conexion_bd)
@@ -20,12 +19,12 @@ mongoose
     console.log(`Database not connected, error ${error}`);
   });
 
-const productsRoutes = require('./api/Routes/routeProduct');
-const providersRoutes = require('./api/Routes/routeProvider');
-
 app.get('/', (req, res) => {
   res.send('server running correctly');
 });
+
+const productsRoutes = require('./api/Routes/routeProduct');
+const providersRoutes = require('./api/Routes/routeProvider');
 
 app.use('/api/products/', productsRoutes());
 app.use('/api/provider/', providersRoutes());
