008bb3e9b0ab663f624e2e7743fb831b39fffc0a
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2022-02-22 01:32:38 -0300

    Se modifica ruta de proveedores linea 17 archivo routeProvider.js

--- api/Routes/Providers/routeProvider.js
+++ api/Routes/Providers/routeProvider.js
@@ -3,20 +3,20 @@ const router = express.Router();
 const controllerProviders = require('../../Controllers/controllerProviders');
 
 module.exports = () => {
-  router.get('/all', controllerProviders.getAllProviders);
-  router.get('/:providerId', controllerProviders.searchById);
-  router.get(
-    '/firstname/:providerFirstName',
-    controllerProviders.searchByfirstName
-  );
-  router.get(
-    '/lastname/:providerLastName',
-    controllerProviders.searchBylastName
-  );
-  router.get('/email/:providerEmail', controllerProviders.searchByEmail);
-  router.put('/:_id', controllerProviders.updateProvider);
-  router.post('/', controllerProviders.addProvider);
-  router.delete('/:providerId', controllerProviders.deleteProvider);
+	router.get('/all', controllerProviders.getAllProviders);
+	router.get('/:providerId', controllerProviders.searchById);
+	router.get(
+		'/firstname/:providerFirstName',
+		controllerProviders.searchByfirstName
+	);
+	router.get(
+		'/lastname/:providerLastName',
+		controllerProviders.searchBylastName
+	);
+	router.get('/email/:providerEmail', controllerProviders.searchByEmail);
+	router.put('/:providerId', controllerProviders.updateProvider);
+	router.post('/', controllerProviders.addProvider);
+	router.delete('/:providerId', controllerProviders.deleteProvider);
 
-  return router;
+	return router;
 };
